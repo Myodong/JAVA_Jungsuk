0c95dc74b8f74b7b172109c7a52cfba7c6f7d6d7
Myodong <gyehd1230@naver.com>
2023-04-29 19:51:14 +0900

    Update Ex3_12.java

--- JAVA_Jungsuk/src/ch03/Ex3_12.java
+++ JAVA_Jungsuk/src/ch03/Ex3_12.java
@@ -6,5 +6,8 @@ class Ex3_12 {
 
 		System.out.printf("%d을 %d로 나누면, %n", x, y); 
 		System.out.printf("몫은 %d이고, 나머지는 %d입니다.%n", x / y, x % y); 
+		
+		System.out.println(10 % 8); // 10을 8 로 나눈 나머지 2차 출력된다
+		System.out.println(10 % -8); // 위와 같은 결과를 출력 한다
 	}
 }
