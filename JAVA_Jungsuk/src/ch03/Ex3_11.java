5d112c8e5cd72ad4d8d2aea0bd591ae4b9380c7c
Myodong <gyehd1230@naver.com>
2023-04-29 18:38:28 +0900

    Update Ex3_11.java

--- JAVA_Jungsuk/src/ch03/Ex3_11.java
+++ JAVA_Jungsuk/src/ch03/Ex3_11.java
@@ -4,5 +4,14 @@ class Ex3_11 {
 		double pi = 3.141592; 
 		double shortPi = Math.round(pi * 1000) / 1000.0;
 		System.out.println(shortPi);
+		
+		
+		//과정
+		System.out.println(pi);
+		System.out.println(pi * 1000);
+		System.out.println( Math.round(pi * 1000));
+		System.out.println( Math.round(pi * 1000) / 1000); // 3  int 결과값
+		System.out.println( Math.round(pi * 1000) / 1000.0); // 3.142 double 결과
+		
 	}
 }
