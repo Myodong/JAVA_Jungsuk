44ad1832502908b13ff7a0bbe59ed679e06f2cac
Myodong <gyehd1230@naver.com>
2023-05-06 15:43:35 +0900

    .

---
+++ JAVA_Jungsuk/src/ch04/Ex4_1.java
@@ -0,0 +1,10 @@
+package ch04;
+class Ex4_1 {
+	public static void main(String args[]) {
+		int score = 80;
+
+		if (score > 60) {
+			System.out.println("�հ��Դϴ�.");
+		}
+	}
+}
---
+++ JAVA_Jungsuk/src/ch04/Ex4_8.java
@@ -0,0 +1,8 @@
+package ch04;
+class Ex4_8 {
+	public static void main(String args[]) {
+		for (int i = 1; i <= 3; i++) { // ��ȣ{}���� ������ 3�� �ݺ�
+			System.out.println("Hello");
+		}
+	}
+}
