7f0f3b2cff852001b0d8db6f7817d9616322864e
Myodong <gyehd1230@naver.com>
2023-05-17 19:51:56 +0900

    Update Ex4_11.java

--- JAVA_Jungsuk/src/ch04/Ex4_11.java
+++ JAVA_Jungsuk/src/ch04/Ex4_11.java
@@ -4,11 +4,12 @@ import java.util.*;
 class Ex4_11 {
 	public static void main(String[] args) { 
 
+		// i =1,2,3,4,5번 {}안의 내용을 반복
 		for(int i=1;i<=5;i++) {
 			for(int j=1;j<=i;j++) {
 				System.out.print("*");
 			}
-			System.out.println();
+			System.out.println(); // 줄바꿈
 		}
 	} // main의 끝
 }
