927b2d1fddaa60202849b460baf6f147232df679
Myodong <gyehd1230@naver.com>
2023-07-20 20:04:02 +0900

    .

---
+++ JAVA_Jungsuk/src/ch05/Ex5_7.java
@@ -0,0 +1,10 @@
+package ch05;
+
+class Ex5_7 {
+	public static void main(String[] args) {
+		System.out.println("¸Å°³º¯¼öÀÇ °³¼ö:"+args.length);
+		for(int i=0;i< args.length;i++) {
+			System.out.println("args[" + i + "] = \""+ args[i] + "\"");
+		}
+	}
+}
